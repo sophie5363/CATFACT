05d8a946358dd455686247250bff92a698942432
Sophie JACQUOT <sophienanterre@gmail.com>
2021-01-01 18:22:40 +0100

    Création du frgamen pokémon List, création de l'Adapter, ajout du RecyclerView dans le fragment_pokemon_list.xml

---
+++ app/src/main/java/com/example/catfact/adapter/PokemonListAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.catfact.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.catfact.PokemonList
+import com.example.catfact.R
+import kotlinx.android.synthetic.main.pokemon_list_item.view.*
+import kotlinx.coroutines.withContext
+
+class PokemonListAdapter(
+    internal var context: Context,
+    internal var pokemonList: List<PokemonList>
+) : RecyclerView.Adapter<PokemonListAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+            internal var img_pokemon:ImageView
+            internal var txt_pokemon:TextView
+            init {
+                img_pokemon = itemView.findViewById(R.id.pokemon_image) as ImageView
+                txt_pokemon = itemView.findViewById(R.id.pokemon_name) as TextView
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.pokemon_list_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        Glide.with(context).load(pokemonList[position]).into(holder.img_pokemon)
+//        holder.txt_pokemon.text = pokemonList[position]
+    }
+
+    override fun getItemCount(): Int {
+       return pokemonList.size
+    }
+
+
+}
