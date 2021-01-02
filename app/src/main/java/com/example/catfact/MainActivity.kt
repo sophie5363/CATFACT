69ce8391e3303ae699fb8253daf779e7b752c2da
Sophie JACQUOT <sophienanterre@gmail.com>
2021-01-02 20:25:09 +0100

    Ajout de la navigation dans un BottomNavView

--- app/src/main/java/com/example/catfact/MainActivity.kt
+++ app/src/main/java/com/example/catfact/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.catfact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_host)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.pokemonList, R.id.quiz, R.id.profil))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
 
 
     }
