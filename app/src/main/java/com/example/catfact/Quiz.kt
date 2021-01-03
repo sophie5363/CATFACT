4b0367855acd5f47700c7d7be6589ddcd2558b65
Sophie JACQUOT <sophienanterre@gmail.com>
2021-01-03 15:57:07 +0100

    QR Code Scanner fonctionnel :) Reste à adapter en français et aux conventions de nommage du projet

--- app/src/main/java/com/example/catfact/Quiz.kt
+++ app/src/main/java/com/example/catfact/Quiz.kt
@@ -1,33 +1,24 @@
 package com.example.catfact
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.TokenWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+import kotlinx.android.synthetic.main.fragment_quiz.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val CAMERA_REQUEST_CODE = 101
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Quiz.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Quiz : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +28,85 @@ class Quiz : Fragment() {
         return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Quiz.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Quiz().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpPermissions()
+        codeScanner()
+    }
+
+
+    private fun codeScanner() {
+        val scannerViewLO = requireView().findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(requireContext(), scannerViewLO)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                requireActivity().runOnUiThread {
+                    tv_textView.text = it.text
                 }
             }
+
+            errorCallback = ErrorCallback {
+                requireActivity().runOnUiThread {
+                    Log.e("Quiz", "Camera initialization error : ${it.message}")
+                }
+            }
+        }
+
     }
+
+
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setUpPermissions() {
+        val permission = ContextCompat.checkSelfPermission(requireActivity(),
+        android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(requireActivity(),
+        arrayOf(android.Manifest.permission.CAMERA),
+        CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(activity, "You need the camera permission to be able to use this app!",
+                    Toast.LENGTH_SHORT).show()
+                }else{
+                    //successful
+                }
+            }
+        }
+    }
+
 }
